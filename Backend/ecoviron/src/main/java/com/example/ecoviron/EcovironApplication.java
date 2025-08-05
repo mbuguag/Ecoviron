592f0e920b53e129bd08625115a2344933e1c20f
mbuguag <mbuguajoseph498@gmail.com>
2025-08-05 18:53:44 +0300

    jar

--- Backend/ecoviron/src/main/java/com/example/ecoviron/EcovironApplication.java
+++ Backend/ecoviron/src/main/java/com/example/ecoviron/EcovironApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = "com.example.ecoviron")
+@SpringBootApplication
 public class EcovironApplication {
 
 	public static void main(String[] args) {
