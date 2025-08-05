fd867f9d3f137103f6405aa8decc2fbf27b0ea1a
mbuguag <mbuguajoseph498@gmail.com>
2025-08-05 20:19:33 +0300

    blog section fix

--- Backend/ecoviron/src/main/java/com/example/ecoviron/dto/PagedResponse.java
+++ Backend/ecoviron/src/main/java/com/example/ecoviron/dto/PagedResponse.java
@@ -1,7 +1,16 @@
 package com.example.ecoviron.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
+@Data
+@AllArgsConstructor
 public class PagedResponse<T> {
     private List<T> content;
     private int pageNumber;
@@ -10,61 +19,4 @@ public class PagedResponse<T> {
     private int totalPages;
     private boolean last;
 
-    public PagedResponse(List<T> content, int pageNumber, int pageSize, long totalElements, int totalPages, boolean last) {
-        this.content = content;
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-        this.totalElements = totalElements;
-        this.totalPages = totalPages;
-        this.last = last;
-    }
-
-    // Getters and Setters
-    public List<T> getContent() {
-        return content;
-    }
-
-    public void setContent(List<T> content) {
-        this.content = content;
-    }
-
-    public int getPageNumber() {
-        return pageNumber;
-    }
-
-    public void setPageNumber(int pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public long getTotalElements() {
-        return totalElements;
-    }
-
-    public void setTotalElements(long totalElements) {
-        this.totalElements = totalElements;
-    }
-
-    public int getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public boolean isLast() {
-        return last;
-    }
-
-    public void setLast(boolean last) {
-        this.last = last;
-    }
 }
